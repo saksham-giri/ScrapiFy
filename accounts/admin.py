cb1a779846c8558bf6e3b3b28911b0b00fcb2986
saksham-giri <168293315+saksham-giri@users.noreply.github.com>
2026-09-08 03:00:39 +0530

    Register custom user model in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (("Scrapify", {"fields": ("phone", "role")}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (("Scrapify", {"fields": ("phone", "role")}),)
+    list_display = ("username", "email", "phone", "role", "is_staff")
